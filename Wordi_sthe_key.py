8953652d6fb444451316ec915213f69db836e7a1
Ankit Kumar Singh <77728943+SinghAnkIT1812@users.noreply.github.com>
2021-04-14 12:19:12 +0530

    Add files via upload
    TCS second question based on words

---
+++ Wordi_sthe_key.py
@@ -0,0 +1,6 @@
+Keyword={"break","case","continue","default","defer","else","for","func","goto","if","map","range","return","struct","type","var"}
+A=input()
+if A in Keyword:
+    print(A + "is a Keyword")
+else:
+    print(A + "is not a Keyword")

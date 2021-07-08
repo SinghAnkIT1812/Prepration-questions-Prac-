f2da3f807d8f2ffe041d3b106eacffc365d0b04d
Ankit Kumar Singh <77728943+SinghAnkIT1812@users.noreply.github.com>
2021-07-08 18:44:50 +0530

    Add files via upload

---
+++ prac1.py
@@ -0,0 +1,13 @@
+av=8
+x=int(input("How many candies you want?"))
+i=1
+while i<=x:
+
+    if i>av:
+        print("we are out of stock")   
+        break  
+    
+
+    print("Candy")    
+    i+=1
+print("Have a good day")

14ca74632014c0da5e1dba2df3dcdef31aea9ba4
Ankit Kumar Singh <77728943+SinghAnkIT1812@users.noreply.github.com>
2021-07-23 12:39:33 +0530

    Update lambda_prac.py

--- lambda_prac.py
+++ lambda_prac.py
@@ -1,14 +1,14 @@
-#is_even=lambda a:a%2==0
-#print(is_even(2))
+is_even=lambda a:a%2==0
+print(is_even(2))
 
-#last_char=lambda a:a[::-1]
+last_char=lambda a:a[::-1]
 
-#print(last_char('ankit'))
+print(last_char('ankit'))
 
 #lambda with if else
 
-#length_s=lambda a: True if len(a)>5 else False
-#print(length_s('chu'))
+length_s=lambda a: True if len(a)>5 else False
+print(length_s('chu'))
 
 def find_pos(l,s):
     for pos, name in enumerate(l):
